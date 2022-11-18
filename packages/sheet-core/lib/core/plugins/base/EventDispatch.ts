bda27a72457868465e54ab0d59a239d19fbb0a93
yukaige <yukaige@bytedance.com>
2022-11-18 13:50:52 +0800

    merge

--- packages/sheet-core/lib/core/plugins/base/EventDispatch.ts
+++ packages/sheet-core/lib/core/plugins/base/EventDispatch.ts
@@ -9,21 +9,15 @@ import { EventConstant } from './event';
 // preData 是为了服用judge得到的值
 type preData = any;
 export interface EventStackType {
-	type: EventZIndex;
-	/**
-	 * 一个用来判断是进入innerFunc的判断方法，
-	 * 返回 false代表是outer，
-	 * 返回true或者任何值，代表inner，并且把该值重新当成inner的入参
-	 */
-	judgeFunc: (e: MouseEvent) => boolean | preData;
-	innerFunc: (
-		e: MouseEvent,
-		preData?: preData,
-	) => void | Array<(e: MouseEvent) => void>;
-	outerFunc?: (
-		e: MouseEvent,
-		preData?: preData,
-	) => void | Array<(e: MouseEvent) => void>;
+  type: EventZIndex;
+  /**
+   * 一个用来判断是进入innerFunc的判断方法，
+   * 返回 false代表是outer，
+   * 返回true或者任何值，代表inner，并且把该值重新当成inner的入参
+   */
+  judgeFunc: (e: MouseEvent) => boolean | preData;
+  innerFunc: (e: MouseEvent, preData?: preData) => void | Array<(e: MouseEvent) => void>;
+  outerFunc?: (e: MouseEvent, preData?: preData) => void | Array<(e: MouseEvent) => void>;
 }
 
 export type setEventType = (type: EventConstant, props: EventStackType) => void;
@@ -31,79 +25,79 @@ export type clearEventType = (type: EventConstant, zIndex: EventZIndex) => void;
 export type dispatchEventType = (type: EventConstant, e: MouseEvent) => void;
 
 export default class EventDispatch {
-	private _this: Base;
-	public name: string;
-	private eventStack: Partial<Record<EventConstant, Array<EventStackType[]>>>;
-	// 结构
-	// eventStack: {
-	//   mouse_move: [
-	//     [
-	//       {
-	//         type: EventZIndex;
-	//         judgeFunc: (e: MouseEvent) => boolean;
-	//         innerFunc: (e: MouseEvent) => void | Array<(e: MouseEvent) => void>;
-	//         outerFunc?: (e: MouseEvent) => void | Array<(e: MouseEvent) => void>;
-	//       }
-	//     ]
-	//   ]
-	// }
-	constructor(_this: Base) {
-		this._this = _this;
-		this.name = PluginTypeEnum.EventDispatch;
-		this.eventStack = {};
-		this._this.setEvent = this.setEvent.bind(this);
-		this._this.dispatchEvent = this.dispatchEvent.bind(this);
-		this._this.clearEvent = this.clearEvent.bind(this);
-	}
+  private _this: Base;
+  public name: string;
+  private eventStack: Partial<Record<EventConstant, Array<EventStackType[]>>>;
+  // 结构
+  // eventStack: {
+  //   mouse_move: [
+  //     [
+  //       {
+  //         type: EventZIndex;
+  //         judgeFunc: (e: MouseEvent) => boolean;
+  //         innerFunc: (e: MouseEvent) => void | Array<(e: MouseEvent) => void>;
+  //         outerFunc?: (e: MouseEvent) => void | Array<(e: MouseEvent) => void>;
+  //       }
+  //     ]
+  //   ]
+  // }
+  constructor(_this: Base) {
+    this._this = _this;
+    this.name = PluginTypeEnum.EventDispatch;
+    this.eventStack = {};
+    this._this.setEvent = this.setEvent.bind(this);
+    this._this.dispatchEvent = this.dispatchEvent.bind(this);
+    this._this.clearEvent = this.clearEvent.bind(this);
+  }
 
-	protected clearEvent(type: EventConstant, subType: EventZIndex) {
-		if (!this.eventStack[type]) {
-			return;
-		}
-		// @ts-ignore
-		this.eventStack[type][subType] = [];
-	}
+  protected clearEvent(type: EventConstant, subType: EventZIndex) {
+    if (!this.eventStack[type]) {
+      return;
+    }
+    // @ts-ignore
+    this.eventStack[type][subType] = [];
+  }
 
-	protected setEvent(type: EventConstant, props: EventStackType) {
-		if (!this.eventStack[type]) {
-			this.eventStack[type] = [];
-		}
-		// @ts-ignore
-		const pointer = this.eventStack[type][props.type];
-		if (pointer) {
-			pointer.push(props);
-		} else {
-			// @ts-ignore
-			this.eventStack[type][props.type] = [props];
-		}
-	}
+  protected setEvent(type: EventConstant, props: EventStackType) {
+    if (!this.eventStack[type]) {
+      this.eventStack[type] = [];
+    }
+    // @ts-ignore
+    const pointer = this.eventStack[type][props.type];
+    if (pointer) {
+      pointer.push(props);
+    } else {
+      // @ts-ignore
+      this.eventStack[type][props.type] = [props];
+    }
+  }
 
-	protected dispatchEvent(type: EventConstant, e: MouseEvent) {
-		const innerFuncArr: ((e: MouseEvent) => void)[] = [];
-		const outerFuncArr: ((e: MouseEvent) => void)[] = [];
-		let isFirst = true;
-		this.eventStack[type]?.forEach((events) => {
-			if (!isFirst) {
-				return;
-			}
-			events.forEach((eventStack) => {
-				if (eventStack.judgeFunc) {
-					const preData = eventStack.judgeFunc(e);
-					if (preData !== false) {
-						isFirst = false;
-						innerFuncArr.push((e: MouseEvent) => {
-							eventStack.innerFunc(e, preData);
-						});
-					} else {
-						if (eventStack.outerFunc) {
-							outerFuncArr.push(eventStack.outerFunc);
-						}
-					}
-				}
-				return false;
-			});
-		});
-		outerFuncArr.flat().forEach((fn) => fn(e));
-		innerFuncArr.flat().forEach((fn) => fn(e));
-	}
+  protected dispatchEvent(type: EventConstant, e: MouseEvent) {
+    const innerFuncArr: ((e: MouseEvent) => void)[] = [];
+    const outerFuncArr: ((e: MouseEvent) => void)[] = [];
+    let isFirst = true;
+    this.eventStack[type]?.forEach((events) => {
+      if (!isFirst) {
+        return;
+      }
+      events.forEach((eventStack) => {
+        if (eventStack.judgeFunc) {
+          const preData = eventStack.judgeFunc(e);
+          if (preData !== false) {
+            isFirst = false;
+            innerFuncArr.push((e: MouseEvent) => {
+              eventStack.innerFunc(e, preData);
+            });
+          } else {
+            if (eventStack.outerFunc) {
+              outerFuncArr.push(eventStack.outerFunc);
+            }
+          }
+        }
+        return false;
+      });
+    });
+    outerFuncArr.flat().forEach((fn) => fn(e));
+    innerFuncArr.flat().forEach((fn) => fn(e));
+  }
 }
